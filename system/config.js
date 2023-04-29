71c7f415fc0f4541ceb6b47db39fce09762256ea
rizkiwibu <98686896+rizkiwibu@users.noreply.github.com>
2023-04-30 00:54:43 +0700

    Update config.js

--- system/config.js
+++ system/config.js
@@ -1,8 +1,8 @@
 const { Function, Scraper } = new (require('@neoxr/neoxr-js'))
 // Owner number
-global.owner = '994408364923'
+global.owner = '6285878836361'
 // Owner name
-global.owner_name = 'Wildan Izzudin'
+global.owner_name = 'rizki'
 // Database name (Default: database)
 global.database = 'bot'
 // Maximum upload file size limit (Default : 100 MB)
@@ -26,7 +26,7 @@ global.timezone = 'Asia/Jakarta'
 // Bot version
 global.version = '2.2.2',
 // Bot name
-global.botname = `© neoxr-bot v${global.version} (Public Bot)`
+global.botname = `© iky-bot v${global.version} (Public Bot)`
 // Footer text
 global.footer = 'ꜱɪᴍᴘʟᴇ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ ᴍᴀᴅᴇ ʙʏ ɴᴇᴏxʀ ッ'
 // Scraper
